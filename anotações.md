620f9c118887fa3b8b4b63d319aa18d92e25387b
Kennedy7Sa <kennedy.arruda@aluno.unip.br>
2026-04-13 20:50:35 -0300

    first

---
+++ anotações.md
@@ -0,0 +1,24 @@
+## Criando o ambiente virtual
+
+- Abra o terminal (cmd)
+
+  ´´´cmd
+
+  python -m venv venv
+
+  ´´´
+
+- Para ativar o ambiente virtual
+
+  ´´´cmd
+
+  venv\Scripts\activate
+
+  ´´´
+  - Para desativar o ambiente virtual
+
+  ´´´cmd
+
+  deactivate
+
+  ´´´
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/contato')
+def contato():
+    return 'Qualquer duvida entre em contato om kennedy'
+
+if __name__ == '__main__':
+    app.run(debug=True) #toda mudança ja é implementada sempre que houver uma edição 
