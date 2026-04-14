f3764d279d0e303e7fc8148e0363e1e6d92d3fad
Kennedy7Sa <kennedy.arruda@aluno.unip.br>
2026-04-14 19:53:38 -0300

    paginas

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('home.html') #usando render_template para chamar o arquivo html 
 
 @app.route('/contato')
 def contato():
-    return 'Qualquer duvida entre em contato om kennedy'
+    return render_template('contato.html')
 
 if __name__ == '__main__':
     app.run(debug=True) #toda mudança ja é implementada sempre que houver uma edição 
