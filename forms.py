4c8ee33a612c0656179bf1cfbf249bfc334b23a7
Kennedy7Sa <kennedyarruda30@gmail.com>
2026-04-17 12:52:04 -0300

    validação dos formularios

--- forms.py
+++ forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm #pra criar o formulario 
-from wtforms import StringField,PasswordField,SubmitField,BooleanField # criar campos do formulario 
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,EmailField # criar campos do formulario 
 from wtforms.validators import DataRequired,length,Email,equal_to #validar campos 
 
 class FormCriarConta(FlaskForm):
     username= StringField('Nome de usuario',validators=[DataRequired()])
-    email= StringField('Email',validators=[DataRequired(),Email()])
+    email= EmailField('Email',validators=[DataRequired()])
     senha= PasswordField('Senha',validators=[DataRequired(),length(6,20)])
     confirmacao= PasswordField('confirmação de senha',validators=[DataRequired(),equal_to('senha')])
     botao_submit_criarConta = SubmitField('Criar conta')
 
 
 class FormLogin(FlaskForm):
-    email= StringField('Digite seu email',validators=[DataRequired(),Email()])
+    email= EmailField('Digite seu email',validators=[DataRequired()])
     senha = PasswordField('Digite sua senha',validators=[DataRequired(),length(6,20)])
     lembrar_dados = BooleanField('Lembrar dados de acesso')
     botao_submit_logar = SubmitField('Fazer login')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, url_for
+from flask import Flask,render_template, url_for,request,flash,redirect
 from forms import FormLogin,FormCriarConta
 
 app = Flask(__name__)
@@ -18,10 +18,22 @@ def contato():
 def usuarios():
     return render_template('usuarios.html',lista_usuarios=lista_usuarios)
 
-@app.route('/login_criarconta')
+@app.route('/login_criarconta',methods=['GET','POST']) #formularios tem que liberar os metodos pra uso como o POST
 def loginCriarConta():
     formLogin = FormLogin()
     formCriarConta = FormCriarConta()
+
+    if formLogin.validate_on_submit and 'botao_submit_logar' in request.form:
+         # para saber qual botão foi clicado importamo o request
+        #exibir mensagem de login bem sucedido (import o flash)
+        flash(f'Login feito com sucesso com o email {formLogin.email.data}','alert-success')
+        # e voltar pra pagina inicial (import o redirect)
+        return redirect(url_for('home'))
+    if formCriarConta.validate_on_submit and 'botao_submit_criarConta' in request.form:
+        flash(f'Conta criada com sucesso com o email {formCriarConta.email.data}','alert-success')
+        return redirect(url_for('home'))
+
+
     return render_template('login_criar_conta.html',formCriarConta=formCriarConta,formLogin=formLogin)
 
 if __name__ == '__main__':
