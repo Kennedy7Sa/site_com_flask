06e2b3d1ee0d778bb0b3038eaf64e3910dc1b567
Kennedy7Sa <kennedyarruda30@gmail.com>
2026-04-23 12:01:50 -0300

    login feito

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a3ff4c3b25f04b163a48caa8afc3e869'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
  
 database = SQLAlchemy(app)
-Bcrypt = Bcrypt(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from comunidadeimpressionadora import routes
--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template,redirect,url_for,flash,request
 from comunidadeimpressionadora.forms import FormCriarConta,FormLogin
-from comunidadeimpressionadora import app , database , Bcrypt
+from comunidadeimpressionadora import app , database , bcrypt
 from comunidadeimpressionadora.models import Usuario,Post
+from flask_login import login_user
 
 lista_usuarios = ['kennedy', 'mateus','jonatan']
 
@@ -24,14 +25,24 @@ def loginCriarConta():
     formCriarConta = FormCriarConta()
 
     if formLogin.validate_on_submit() and 'botao_submit_logar' in request.form:
-         # para saber qual botão foi clicado importamo o request
-        #exibir mensagem de login bem sucedido (import o flash)
-        flash(f'Login feito com sucesso com o email {formLogin.email.data}','alert-success')
-        # e voltar pra pagina inicial (import o redirect)
-        return redirect(url_for('home'))
+        usuario = Usuario.query.filter_by(email=formLogin.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha,formLogin.senha.data):
+        
+            login_user(usuario,remember=formLogin.lembrar_dados.data)
+            # para saber qual botão foi clicado importamo o request
+            #exibir mensagem de login bem sucedido (import o flash)
+            flash(f'Login feito com sucesso com o email {formLogin.email.data}','alert-success')
+            # e voltar pra pagina inicial (import o redirect)
+            return redirect(url_for('home'))
+        else:
+            flash('Falha ao logar email ou senha incorretos','alert-danger')
+    
+    
+    
+    
     if formCriarConta.validate_on_submit() and 'botao_submit_criarConta' in request.form:
         #criptografando a senha 
-        senha_cript = Bcrypt.generate_password_hash(formCriarConta.senha.data) 
+        senha_cript = bcrypt.generate_password_hash(formCriarConta.senha.data) 
         #criar um usuario(estancia da classe usuario)
         usuario1 = Usuario(username = formCriarConta.username.data,email=formCriarConta.email.data,senha=senha_cript)
         #adciono sessao 
