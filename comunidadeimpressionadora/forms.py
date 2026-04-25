9e5e1d026a6f777c30d8b8ee8eadb2ca1677e903
Kennedy7Sa <kennedyarruda30@gmail.com>
2026-04-25 09:17:54 -0300

    formulario de edição de perfil

--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm #pra criar o formulario
 from wtforms import StringField,PasswordField,SubmitField,BooleanField # criar campos do formulario 
 from wtforms.validators import DataRequired,Length,Email,ValidationError,EqualTo #validar campos 
 from comunidadeimpressionadora.models import Usuario
+from flask_login import current_user
 
 class FormCriarConta(FlaskForm):
     username= StringField('Nome de usuario',validators=[DataRequired()])
@@ -22,3 +23,15 @@ class FormLogin(FlaskForm):
     senha = PasswordField('Digite sua senha',validators=[DataRequired(),Length(6,20)])
     lembrar_dados = BooleanField('Lembrar dados de acesso')
     botao_submit_logar = SubmitField('Fazer login')
+
+
+class FormEditarPerfil(FlaskForm):
+    username= StringField('Nome de usuario',validators=[DataRequired()])
+    email= StringField('Digite seu email',validators=[DataRequired(),Email()])
+    botao_submit_editarPerfil = SubmitField('Confirmar edição')
+    
+    def validate_email(self,email):
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email= email.data).first()
+            if usuario:
+                raise ValidationError('Esse email já esxiste ! coloque outro email para edição')
--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template,redirect,url_for,flash,request
-from comunidadeimpressionadora.forms import FormCriarConta,FormLogin
+from comunidadeimpressionadora.forms import FormCriarConta,FormLogin,FormEditarPerfil
 from comunidadeimpressionadora import app , database , bcrypt
 from comunidadeimpressionadora.models import Usuario,Post
 from flask_login import login_user,logout_user , current_user,login_required
@@ -74,11 +74,30 @@ def sair():
 @app.route('/perfil')
 @login_required
 def perfil():
-    return render_template('perfil.html')
+    foto_perfil = url_for('static',filename=f'fotos_perfil/{current_user.foto_perfil}')
+    return render_template('perfil.html',foto_perfil=foto_perfil)
 
 @app.route('/post/criar')
 @login_required
 def criar_post():
     return render_template('criarpost.html')
+
+@app.route('/perfil/editar',methods=['GET','POST'])
+@login_required
+def editarPerfil():
+    form = FormEditarPerfil()
+    if form.validate_on_submit():
+        current_user.email = form.email.data
+        current_user.username = form.username.data
+        database.session.commit()
+        flash('Perfil atualizado com sucesso ','alert-successes')
+        return redirect(url_for('perfil'))
+    elif request.method == 'GET':
+        form.email.data = current_user.email
+        form.username.data = current_user.username
+
+
+    foto_perfil = url_for('static',filename=f'fotos_perfil/{current_user.foto_perfil}')
+    return render_template('editarperfil.html',foto_perfil=foto_perfil,form = form)
     
 
